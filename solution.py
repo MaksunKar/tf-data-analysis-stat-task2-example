5f33aefcf87bb3cc660dac6e1b711cd414365415
MaksunKar <130152425+MaksunKar@users.noreply.github.com>
2023-04-07 20:26:16 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,9 +4,9 @@ import numpy as np
 from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 790154026 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    mean1 = 2 / 92 ** 2 * (gamma.ppf((1 - p) / 2, x.size) / x.size + x.mean() - 1 / 2)
-    mean2 = 2 / 92 ** 2 * (gamma.ppf((1 + p) / 2, x.size) / x.size + x.mean() - 1 / 2)
+    mean1 = 2 / 89 ** 2 * (gamma.ppf((1 - p) / 2, x.size) / x.size + x.mean() - 1 / 2)
+    mean2 = 2 / 89 ** 2 * (gamma.ppf((1 + p) / 2, x.size) / x.size + x.mean() - 1 / 2)
     return mean1, mean2
